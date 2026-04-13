5ba8855ea200aac84655466ab81d1d32f90ebb71
Ivan Dukhota <ivan.dukhota@gmail.com>
2026-04-13 15:36:31 +0300

    Updated whole project for CI

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from apps.common.views import health_check
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("apps.users.urls")),
+    path('api/v1/health/', health_check),
 ]
